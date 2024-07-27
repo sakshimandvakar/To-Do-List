8143e09e4eb6d0f169624901a3f548795e0be514
Sakshi Mandvakar <sakshimandvakar6094@gmail.com>
2024-07-27 14:50:48 +0530

    initial commit

--- script.js
+++ script.js
@@ -9,5 +9,32 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveData();
 }
+
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML = localStorage.getItem("data")
+}
+showtask();
